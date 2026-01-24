a4841b2a09df669ab44349b682cc155fd429cad4
prolifel <clement.085w@gmail.com>
2026-01-24 10:11:12 +0700

    feat: update api to use mysql2

--- app/api/domains/route.ts
+++ app/api/domains/route.ts
@@ -1,33 +1,30 @@
-import { Prisma } from "@/generated/prisma/client"
-import { prisma } from "@/lib/prisma"
+import { createDomain, getAllDomains, getDomainByDomain } from "@/lib/repo/domain"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(request: NextRequest) {
     const q = request.nextUrl.searchParams.get('q')
-    const domains = await prisma.domain.findMany(
-        q ? { where: { domain: q } } : undefined
-    )
-    return NextResponse.json(domains)
+
+    let res
+    if (q == null || q === '') {
+        res = await getAllDomains()
+    } else {
+        res = await getDomainByDomain(q)
+    }
+
+    return NextResponse.json({
+        message: res === null ? "domain not found" : "success",
+        data: res
+    }, {
+        status: res === null ? 404 : 200
+    })
 }
 
 export async function POST(request: NextRequest) {
     try {
         const json = await request.json()
-        const domain = await prisma.domain.create({
-            data: json
-        })
+        const domain = await createDomain(json.domain)
         return NextResponse.json(domain)
     } catch (error: any) {
-        if (error instanceof Prisma.PrismaClientKnownRequestError) {
-            if (error.code === 'P2002') {
-                return NextResponse.json({
-                    message: "Domain already exist!"
-                }, {
-                    status: 409
-                })
-            }
-        }
-
         console.error(error)
         return NextResponse.json({
             message: "Internal server error"
--- lib/repo/domain.ts
+++ lib/repo/domain.ts
@@ -1,29 +1,55 @@
 import { pool } from '@/lib/shared/db';
-import { RowDataPacket } from 'mysql2';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
 
-import { prisma } from "../prisma"
-
-export interface Domain extends RowDataPacket {
+export interface DomainRow extends RowDataPacket {
+    domain: string
+    id: number
+}
+export interface Domain {
     domain: string
     id: number
 }
 
-export async function getDomains(query: string | null) {
-    return await prisma.domain.findFirst(
-        query ? { where: { domain: query } } : undefined
-    )
-}
-
 export async function getDomainByDomain(query: string): Promise<Domain | null> {
     try {
-        const [rows] = await pool.query<Domain[]>(
-            `select domain from domains where domain = ?`,
+        const [rows] = await pool.query<DomainRow[]>(
+            `select id, domain from domains where domain = ?`,
             [query]
         )
 
         return rows.length > 0 ? rows[0] : null
     } catch (error) {
         console.error(error);
+        return null
+    }
+}
+
+export async function getAllDomains(): Promise<Domain[]> {
+    try {
+        const [rows] = await pool.query<DomainRow[]>(
+            `select domain, id from domains`
+        )
+
+        return rows
+    } catch (error) {
+        console.error(error)
+        return []
+    }
+}
+
+export async function createDomain(domainName: string): Promise<Domain | null> {
+    try {
+        const [res] = await pool.execute<ResultSetHeader>(
+            `insert into domains(domain) values (?)`,
+            [domainName]
+        )
+
+        return {
+            id: res.insertId,
+            domain: domainName
+        }
+    } catch (error) {
+        console.error(error)
         return null
     }
 }
